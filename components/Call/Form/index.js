c476d2ce574f957cc61d639ffed6cce51c4b74d0
high5dev <high5dev621@hotmail.com>
2021-12-15 17:43:43 +0100

    design

--- components/Call/Form/index.js
+++ components/Call/Form/index.js
@@ -1,8 +1,7 @@
 import { React, useState } from "react";
-import { Field, useFormik } from "formik";
 import "react-phone-number-input/style.css";
 import PhoneInput from "react-phone-number-input";
-
+import { useFormik } from 'formik';
 const RepportForm = () => {
   const [value, setValue] = useState();
   const formik = useFormik({
@@ -20,20 +19,23 @@ const RepportForm = () => {
     },
   });
   return (
-    <form onSubmit="">
+    <form onSubmit={formik.handleSubmit}>
       <label htmlFor="lastname">Nom</label>
       <input
         id="lastname"
-        placeholder="Saisir votre nom (prénom)"
         name="lastname"
+        placeholder="Saisir votre nom (prénom)"
         type="text"
+        onChange={formik.handleChange}
+        value={formik.values.lastname}
       />
 
       <label htmlFor="phone">Téléphone</label>
-      <PhoneInput defaultCountry="FR" value={value} onChange={setValue} />
+      <PhoneInput placeholder="06 00 00 00 00" defaultCountry="FR" value={value} onChange={setValue} />
 
       <div className="select">
-        <select id="type" name="type" type="select">
+        <select id="type" name="type" onChange={formik.handleChange}
+        value={formik.values.type}>
           <option value="" label="Type" />
           <option value="Psychologique" label="Psychologique" />
           <option value="Verbale" label="Verbale" />
@@ -43,8 +45,10 @@ const RepportForm = () => {
           <option value="Plusieurs sortes" label="Plusieurs sortes" />
         </select>
       </div>
+
       <div className="select">
-        <select id="concerning" name="concerning" type="select">
+        <select id="concerning" name="concerning" onChange={formik.handleChange}
+        value={formik.values.concerning}>
           <option value="" label="Qui est concerné ?" />
           <option value="Femme" label="Femme" />
           <option value="Homme" label="Homme" />
@@ -54,16 +58,20 @@ const RepportForm = () => {
           <option value="Enfant (H)" label="Enfant (M)" />
         </select>
       </div>
+
       <div className="select">
-        <select id="child" name="child" type="select">
+        <select id="child" name="child" onChange={formik.handleChange}
+        value={formik.values.child}>
           <option value="" label="Enfant(s) dans le foyer ?" />
           <option value="Oui" label="Oui" />
           <option value="Non" label="Non" />
           <option value="Non applicable" label="Non applicable" />
         </select>
       </div>
+
       <div className="select">
-        <select id="fromwho" placeholder="Origine" name="fromwho" type="select">
+        <select id="fromwho" placeholder="Origine" name="fromwho"  onChange={formik.handleChange}
+        value={formik.values.fromwho}>
           <option value="" label="Origine" />
           <option value="Moi-même" label="Moi-même" />
           <option value="Témoin" label="Témoin" />
@@ -73,7 +81,7 @@ const RepportForm = () => {
             value="Professionnel de santé"
             label="Professionnel de santé"
           />
-        </select>{" "}
+        </select>
       </div>
 
       <label htmlFor="details">Information complémentaire</label>
